4706b36a3dd1e276024aa36d3de8e3938cae3040
ge-arty <115204381+ge-arty@users.noreply.github.com>
2022-11-30 23:58:01 +0400

    update

--- src/App.js
+++ src/App.js
@@ -5,38 +5,80 @@ import Header from "./Header";
 import Search from "./Search";
 import User from "./User";
 import { useEffect, useState } from "react";
+import { Context } from "./Context";
 
 function App() {
   const [username, setUsername] = useState("octocat");
+  const [searchInp, setSearchInput] = useState("");
   const [data, setData] = useState();
   const [show, setShow] = useState(false);
+  const [theme, setTheme] = useState(false);
+  const [fetched, setFetched] = useState(false);
+
+  function getInfo() {
+    fetch(`https://api.github.com/users/${username}`)
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error("something went wrong");
+      })
+      .then((responseJson) => {
+        setData(responseJson);
+        setShow(true);
+      })
+      .catch((error) => {
+        console.log(error);
+        setFetched(true);
+      });
+  }
   useEffect(() => {
-    axios.get(`https://api.github.com/users/${username}`).then((response) => {
-      console.log(response.data);
-      setData(response.data);
-      setShow(true);
-    });
-  }, []);
+    // axios.get(`https://api.github.com/users/${username}`).then((response) => {
+    //   console.log(response.data);
+    //   setData(response.data);
+    //   setShow(true);
+    // });
+    getInfo();
+  }, [username]);
+  function changeUser(event) {
+    setSearchInput(event.target.value);
+  }
+  function searchClick() {
+    setUsername(searchInp);
+    setFetched(false);
+  }
+  function changeColor() {
+    setTheme((current) => !current);
+  }
   return (
-    <div className="app">
-      <Header />
-      <Search />
-      {show && (
-        <User
-          src={data.avatar_url}
-          name={data.name}
-          login={data.login}
-          joinDate={data.created_at}
-          bio={data.bio}
-          publicRepos={data.public_repos}
-          followers={data.followers}
-          following={data.following}
-          location={data.location}
-          twitter={data.twitter_username}
-          company={data.company}
-          blog={data.blog}
-        />
-      )}
+    <div
+      className="app"
+      style={
+        theme == true
+          ? { backgroundColor: "#141D2F" }
+          : { backgroundColor: "#F6F8FF" }
+      }
+    >
+      <Context.Provider value={{ switch: theme, fetched: fetched }}>
+        <Header changeColor={changeColor} />
+        <Search onChange={changeUser} Click={() => searchClick()} />
+        {show && (
+          <User
+            src={data.avatar_url}
+            name={data.name}
+            login={data.login}
+            joinDate={data.created_at}
+            bio={data.bio}
+            publicRepos={data.public_repos}
+            followers={data.followers}
+            following={data.following}
+            location={data.location}
+            twitter={data.twitter_username}
+            company={data.company}
+            blog={data.blog}
+          />
+        )}
+      </Context.Provider>
     </div>
   );
 }
--- src/Search.js
+++ src/Search.js
@@ -1,18 +1,38 @@
-import React from "react";
+import React, { useContext } from "react";
 import searchIcon from "./images/search-icon.png";
+import { Context } from "./Context";
 
-export default function Search() {
+export default function Search(props) {
+  let theme = useContext(Context);
   return (
-    <div className="search-container">
+    <div
+      style={
+        theme.switch == true
+          ? { backgroundColor: "#1E2A47" }
+          : { backgroundColor: "#FEFEFE" }
+      }
+      className="search-container"
+    >
       <img src={searchIcon} alt="search-icon" />
 
       <input
+        style={
+          theme.switch == true
+            ? { backgroundColor: "#1E2A47", color: "#FEFEFE" }
+            : { backgroundColor: "#FEFEFE", color: "#1E2A47" }
+        }
+        onChange={props.onChange}
         placeholder="Search GitHub username…"
         className="search-input"
+        id={theme.switch == true ? "search-input-id" : ""}
         type="text"
       />
-      <p className="search-noresult-text">no results</p>
-      <button className="search-btn">Search</button>
+      <p className="search-noresult-text">
+        {theme.fetched ? "no results" : ""}
+      </p>
+      <button onClick={props.Click} className="search-btn">
+        Search
+      </button>
     </div>
   );
 }
